091998f274c76309d41f05b1f60d2309883ea734
Natalie Hummel <natoms225@gmail.com>
2020-05-10 11:09:41 -0500

    POST working

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+//GET all feedback
 router.get('/', (req, res) => {
     console.log('GET /api/feedback');
     pool.query('SELECT * from "feedback";')
@@ -12,5 +13,21 @@ router.get('/', (req, res) => {
         res.sendStatus(500);
     });
 })
+
+//POST new feedback
+router.post('/', (req, res) => {
+    console.log('POST /api/feedback');
+    const newFeedback = req.body;
+    const sqlText = `INSERT INTO "feedback" (feeling, understanding, support, comments) VALUES ($1, $2, $3, $4)`;
+    pool.query(sqlText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments])
+        .then((result) => {
+            console.log(`Added feedback to the database ${newFeedback}`)
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log(`Error masking database: ${error}`);
+            res.sendStatus(500);
+        })
+})
+
 
 module.exports = router;
--- src/components/ReviewFeedback/ReviewFeedback.jsx
+++ src/components/ReviewFeedback/ReviewFeedback.jsx
@@ -1,6 +1,37 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
-class ReviewFeedback extends Component{
+class ReviewFeedback extends Component {
+    state = {
+        feeling: '',
+        understanding: '',
+        support: '',
+        comments: ''
+    }
+
+    addFeedback = () => {
+        axios({
+            url: '/api/feedback',
+            method: 'POST',
+            data: this.state
+        }).then(response =>{
+            console.log(response);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    clickHandler = () => {
+        console.log('in clickHandler')
+        const updatedState = {
+            feeling: Number(this.props.feedback[0]),
+            understanding: Number(this.props.feedback[1]),
+            support: Number(this.props.feedback[2]),
+            comments: this.props.feedback[3]
+        }
+        this.setState(updatedState, this.addFeedback)
+    }
+
     render(){
         return(
             <>
@@ -9,7 +40,7 @@ class ReviewFeedback extends Component{
             <h3>Understaning: {this.props.feedback[1]}</h3>
             <h3>Support: {this.props.feedback[2]}</h3>
             <h3>Comments: {this.props.feedback[3]}</h3>
-            <button>Submit</button>
+            <button onClick={this.clickHandler}>Submit</button>
             </>
         )
     }
